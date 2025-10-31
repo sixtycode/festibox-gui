379f4f17da06ba3c9d4613cd9ba03a7d35ed17f2
sixtycode <sagaraperkasa.work@gmail.com>
2025-10-31 11:34:45 +0700

    chore: nuxt vite-pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config'
+
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   routeRules: {
     // prerender index route by default
     '/': { prerender: true },
   },
+
+  modules: ['@vite-pwa/nuxt'],
+  pwa: {
+    /* PWA options */
+  }
 });
